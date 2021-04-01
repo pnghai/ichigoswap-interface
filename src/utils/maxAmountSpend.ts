bf00d1c04477232ab5a331f98ec05ea132694b26
hai <pnghai@gmail.com>
2021-04-02 01:47:33 +0700

    change to ichigoswap-sdk

--- src/utils/maxAmountSpend.ts
+++ src/utils/maxAmountSpend.ts
@@ -1,4 +1,4 @@
-import { CurrencyAmount, ETHER, JSBI } from '@pancakeswap-libs/sdk'
+import { CurrencyAmount, ETHER, JSBI } from '@pnghai/ichigoswap-sdk'
 import { MIN_ETH } from '../constants'
 
 /**
